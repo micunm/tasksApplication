71d5657579b6f740c85f4970ee9935a3f952f2b3
micunm <micun@o2.pl>
2018-08-11 23:03:57 +0200

    Tasks applicstion deployment

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -3,9 +3,11 @@ package com.crud.tasks;
 import com.crud.tasks.domain.TaskDto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TasksApplication {
+public class TasksApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 /*		TaskDto taskDto=new TaskDto(
@@ -19,6 +21,11 @@ public class TasksApplication {
 
 		System.out.println(id+" "+title+" "+content);*/
 		SpringApplication.run(TasksApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TasksApplication.class);
 	}
 }
 
