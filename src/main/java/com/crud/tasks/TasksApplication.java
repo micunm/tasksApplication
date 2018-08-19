5d0d549f7d3d66efb42283c7d2515efb176b4088
micunm <micun@o2.pl>
2018-08-19 13:45:38 +0200

    Tasks application task 18.2 mod2

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -1,10 +1,10 @@
 package com.crud.tasks;
 
-import com.crud.tasks.domain.TaskDto;
+//import com.crud.tasks.domain.TaskDto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
+//import org.springframework.boot.builder.SpringApplicationBuilder;
+//import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class TasksApplication {
