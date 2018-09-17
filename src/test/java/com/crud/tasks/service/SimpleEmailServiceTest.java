13e1f8ea923e8c1361379f11141598f7cf43869c
micunm <micun@o2.pl>
2018-09-17 22:01:53 +0200

    Tasks application task 19.3 email

---
+++ src/test/java/com/crud/tasks/service/SimpleEmailServiceTest.java
@@ -0,0 +1,12 @@
+package com.crud.tasks.service;
+
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(MockitoJUnitRunnernitRunner.class)
+public class SimpleEmailServiceTest {
+
+}
