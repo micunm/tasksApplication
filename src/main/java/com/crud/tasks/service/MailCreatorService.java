23a790bb5aac91e1e9d04e5bdbc48b9afbadd77d
micunm <micun@o2.pl>
2018-10-14 20:37:58 +0200

    Tasks application task 24.3 thymeleaf conditional exp

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -8,6 +8,9 @@ import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class MailCreatorService {
 
@@ -27,17 +30,25 @@ public class MailCreatorService {
     private CompanyConfig companyConfig;*/
 
     public String buildTrelloCardEmail(String message) {
+
+        List<String> functionality=new ArrayList<>();
+        functionality.add("You can manage your tasks");
+        functionality.add("Provides connection with Trello Account");
+        functionality.add("Application allows sending tasks to Trello");
+
         Context context = new Context();
         context.setVariable("message", message);
         context.setVariable("tasks_url", "https://micunm.github.io/");
         context.setVariable("button", "Visit website");
         context.setVariable("godbye_msg","Best Regards,");
-
-       context.setVariable("admin_name", adminConfig.getAdminName());
-       context.setVariable("company_name", companyConfig.getCompanyName() );
+//        context.setVariable("admin_name", adminConfig.getAdminName());
+        context.setVariable("admin_config", adminConfig);
+        context.setVariable("company_name", companyConfig.getCompanyName() );
         context.setVariable("company_email", companyConfig.getCompanyEmail());
         context.setVariable("company_phone", companyConfig.getCompanyPhone());
         context.setVariable("company_goal", companyConfig.getCompanyGoal());
+        context.setVariable("show_button", false);
+        context.setVariable("is_friend", true);
 
         return templateEngine.process("mail/created-trello-card-mail", context);
     }
