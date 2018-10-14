1503fda721268465e310907482c582543689ad17
micunm <micun@o2.pl>
2018-10-14 18:34:54 +0200

    Tasks application task 24.4 mod5

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -1,5 +1,6 @@
 package com.crud.tasks.service;
 
+import com.crud.tasks.config.AdminConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -8,6 +9,10 @@ import org.thymeleaf.context.Context;
 
 @Service
 public class MailCreatorService {
+
+    @Autowired
+    private AdminConfig adminConfig;
+
     @Autowired
     @Qualifier("templateEngine")
     private TemplateEngine templateEngine;
@@ -20,11 +25,11 @@ public class MailCreatorService {
     public String buildTrelloCardEmail(String message) {
         Context context = new Context();
         context.setVariable("message", message);
-        context.setVariable("task_url", "http://localhost:8080/crud");
+        context.setVariable("tasks_url", "http://localhost:8080/crud");
         context.setVariable("button", "Visit website");
 
-/*        context.setVariable("admin_name", adminConfig.getAdminName());
-        context.setVariable("company_name", companyConfig.getCompanyName() );
+       context.setVariable("admin_name", adminConfig.getAdminName());
+ /*       context.setVariable("company_name", companyConfig.getCompanyName() );
         context.setVariable("company_email", companyConfig.getCompanyEmail());
         context.setVariable("company_phone", companyConfig.getCompanyPhone());*/
 
