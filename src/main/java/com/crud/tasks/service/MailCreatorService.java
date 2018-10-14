0b7f6b07d19fad1a1725fc90991b155eb5f6cf5a
micunm <micun@o2.pl>
2018-10-14 18:56:22 +0200

    Tasks application task 24.4 mod6

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -1,6 +1,7 @@
 package com.crud.tasks.service;
 
 import com.crud.tasks.config.AdminConfig;
+import com.crud.tasks.config.CompanyConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -14,6 +15,9 @@ public class MailCreatorService {
     private AdminConfig adminConfig;
 
     @Autowired
+    private CompanyConfig companyConfig;
+
+    @Autowired
     @Qualifier("templateEngine")
     private TemplateEngine templateEngine;
 
@@ -25,13 +29,15 @@ public class MailCreatorService {
     public String buildTrelloCardEmail(String message) {
         Context context = new Context();
         context.setVariable("message", message);
-        context.setVariable("tasks_url", "http://localhost:8080/crud");
+        context.setVariable("tasks_url", "https://micunm.github.io/");
         context.setVariable("button", "Visit website");
+        context.setVariable("godbye_msg","Best Regards,");
 
        context.setVariable("admin_name", adminConfig.getAdminName());
- /*       context.setVariable("company_name", companyConfig.getCompanyName() );
+       context.setVariable("company_name", companyConfig.getCompanyName() );
         context.setVariable("company_email", companyConfig.getCompanyEmail());
-        context.setVariable("company_phone", companyConfig.getCompanyPhone());*/
+        context.setVariable("company_phone", companyConfig.getCompanyPhone());
+        context.setVariable("company_goal", companyConfig.getCompanyGoal());
 
         return templateEngine.process("mail/created-trello-card-mail", context);
     }
